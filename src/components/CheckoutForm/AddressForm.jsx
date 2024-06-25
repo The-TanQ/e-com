6cfe00f6fb8b49e4777ebaa0edca3466b68a176c
The-TanQ <tisetsoramolobeng@gmail.com>
2024-06-25 14:59:01 +0200

    Fixed eslint issues

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -44,7 +44,7 @@ const AddressForm = ({ checkoutToken, next }) => {
 
   useEffect(() => {
     fetchShippingCountries(checkoutToken.id)
-  }, []);
+  }, [checkoutToken.id]);
 
   useEffect(() => {
     if(shippingCountry) fetchSubdivisions(shippingCountry)
@@ -52,7 +52,7 @@ const AddressForm = ({ checkoutToken, next }) => {
   
   useEffect(() => {
     if(shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision)
-  }, [shippingSubdivision]);
+  }, [checkoutToken.id, shippingCountry, shippingSubdivision]);
 
   return (
     <>
